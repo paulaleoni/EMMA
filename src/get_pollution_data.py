a9e068ce494c5c819e45f80d948938020504534b
Paula Leoni <plbeck@web.de>
2022-04-27 11:42:30 +0200

    making csv

--- src/get_pollution_data.py
+++ src/get_pollution_data.py
@@ -1,15 +1,39 @@
 
 from pathlib import Path
+from zipfile import ZipFile
 import xarray as xr
-import rioxarray as rxr
 import geopandas as gpd
+import pandas as pd
 
-wd = Path.cwd()
 
-shp_file = wd.parent/'data'/'shp'/'Kenya.zip'
+if __name__ == "__main__":
+    # load shapefile
+    wd = Path.cwd()
+    shp_file = wd.parent/'data'/'shp'/'Kenya.zip'
+    Kenya_regions = gpd.read_file(shp_file)
+    Kenya = gpd.GeoDataFrame(index=[0],geometry=[Kenya_regions['geometry'].unary_union])
 
-# load shapefile in geopandas dataframe
-Kenya_regions = gpd.read_file(shp_file)
+    # define zip
+    zip_path = 'Pollution_Monthly.zip'
+    zip_file = ZipFile(f'{wd.parent}/data/satellite/{zip_path}')
+    # define time frame
+    years = [2012 + x for x in range(9)]
+    months = [str(0) + str(x) for x in range(1,10)]
+    months.extend(['10','11','12'])
+    # empty dataframe
+    df = pd.DataFrame(columns=['lon','lat'])
+    # loop 
+    for y in years: 
+        for m in months:
+            #print(y,m)
+            # filename
+            file = zip_file.open(f'Monthly/V5GL02.HybridPM25.Global.{y}{m}-{y}{m}.nc')
+            # open data
+            data= xr.open_dataset(file)
+            # save in df and clipping Kenya
+            dfym = data.rio.write_crs(4326, inplace=True).rio.clip(geometries=Kenya.geometry,crs= Kenya.crs ,from_disk=True, drop=True,all_touched=True).GWRPM25.to_dataframe(f'pol{y}{m}').reset_index().drop('spatial_ref', axis=1)
+            # merge
+            df = pd.merge(df, dfym, on=['lon','lat'], how='outer')
 
-Kenya = gpd.GeoDataFrame(index=[0],geometry=[Kenya_regions['geometry'].unary_union])
-
+    # export to csv
+    df.to_csv(wd.parent/'data'/'satellite'/'pollution_raw.csv', index=False)
