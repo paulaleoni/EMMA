fd3876a2f140408dd527a8c97528ce70bf6126f2
Paula Leoni <plbeck@web.de>
2022-08-08 11:38:05 +0200

    transformer stats added

--- src/descriptive_stats.py
+++ src/descriptive_stats.py
@@ -3,9 +3,12 @@ make summary table
 '''
 import pandas as pd
 from pathlib import Path
+import geopandas as gpd
+from shapely import wkt
 import numpy as np
 
 wd = Path.cwd()
+path_stats = wd.parent/'out'/'results'
 
 # load data
 df = pd.read_csv(wd.parent/'out'/'data'/'dataset_yearly.csv')
@@ -13,7 +16,10 @@ df = pd.read_csv(wd.parent/'out'/'data'/'dataset_yearly.csv')
 # prepare data
 df = df[df.pop_dens != 0] # drop grid cells with no population
 df = df[df.nl.notnull()]
+df = df[df.date_first_vend_prepaid.notnull()]
+df = df[df.dist_tr <= 0.02]
 
+'''
 # group grid cells by
 # nothing if neither lmcp, nonlmcp, preexisting
 df['group'] = np.nan
@@ -24,23 +30,37 @@ df.loc[(df.len_lmcp >= df.len_nonlmcp) & (df.len_lmcp >=df.len_preexisting) & df
 df.loc[(df.len_nonlmcp >= df.len_lmcp) & (df.len_nonlmcp >= df.len_preexisting) & df.group.isnull(), 'group'] = 'nonlmcp'
 # #preexisting > #lmcp + #nonlmcp
 df.loc[(df.len_preexisting >= df.len_lmcp) & (df.len_preexisting >= df.len_nonlmcp) & df.group.isnull(), 'group'] = 'preexisting'
-
+'''
 
 # https://stackoverflow.com/questions/54466196/descriptive-statistics-in-python-with-pandas-with-std-in-parentheses
 idx = pd.IndexSlice
-df_desc = (df[df.year.isin([2015,2020])]).groupby(['group','year'])[['nl','pol','pop_dens']].describe()
+years = [x for x in range(2014,2020+1)]
+df_desc = (df[df.year.isin(years)]).groupby(['year'])[['nl','pol','pop_dens']].describe()
 
 df_desc = df_desc.loc[idx[:],idx[:,["mean", "std"]]].T
 df_desc.loc[idx[:,["std"]],idx[:]] = df_desc.loc[idx[:,["std"]],idx[:]].applymap(lambda x: "("+"{:.2f}".format(x)+")")
 
 df_desc.loc[idx[:,["mean"]],idx[:]] = df_desc.loc[idx[:,["mean"]],idx[:]].applymap(lambda x: "{:.2f}".format(x))
 
-groups = df['group'].unique().tolist()
-df_desc.loc['pop_dens',pd.IndexSlice[groups,[2020]]] = "-"
+not2015 = [x for x in years if x != 2015]
+df_desc.loc['pop_dens', not2015] = "-"
 
 df_desc=df_desc.reset_index(drop=True)
 
-df_new = pd.DataFrame(df_desc.values, index=["nightlight", "", "pollution", "",'population',"density"], columns=pd.MultiIndex.from_tuples(df_desc.columns))
+df_new = pd.DataFrame(df_desc.values, index=["nightlight", "", "pollution", "",'population',"density"], columns=df_desc.columns)
 
-with open(wd.parent/'out'/'results'/'stats.tex','w') as tf:
-    tf.write(df_new.style.to_latex(position='H', caption='descriptive statistics by group and year', hrules=True,  label='tab:stats', environment='longtable', multicol_align='c'))
+col_align = 'l' + len(years)*'c'
+
+with open(path_stats/'stats.tex','w') as tf:
+    tf.write(df_new.style.to_latex(position='H', caption='descriptive statistics by group and year', hrules=True,  label='tab:stats', environment='longtable', multicol_align='c', column_format=col_align))
+
+
+### transformer statistics
+df_trans = df[['geometry_transformer','county']].drop_duplicates().reset_index(drop=True).dropna()
+gdf_trans = gpd.GeoDataFrame(df_trans, geometry=df_trans['geometry_transformer'].apply(wkt.loads))
+
+tab_counties = pd.DataFrame(gdf_trans.county.value_counts()).rename(columns={'county':'No'})
+tab_counties.index = tab_counties.index.str.capitalize()
+
+with open(path_stats/'tab_counties.tex','w') as tf:
+    tf.write(tab_counties.style.to_latex(position='H', caption='number of transformers per county', hrules=True,  label='tab:counties', column_format='lc',environment='longtable'))
